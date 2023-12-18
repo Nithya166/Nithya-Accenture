5a573e5ac792485a70c01be9216a166e1b656dce
NithyaKG <142255002+Nithya166@users.noreply.github.com>
2023-12-18 22:51:10 +0530

    Add files via upload

---
+++ Greatest.java
@@ -0,0 +1,23 @@
+public class Greatest {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 6;
+
+      if(num1>num2 && num1>num3) {
+    	  System.out.println("The greatest num is:"+num1);
+    }
+      else if(num2>num1 && num2>num3)
+    {
+    	System.out.println("The greatest num is:"+num2);
+    }
+    else
+    {
+    	System.out.println("The greatest num is:"+num3);
+    }
+    }
+}
+
+    
+
