fef3577e7a8384e9a910c4f828dcebf2c84071f9
NithyaKG <142255002+Nithya166@users.noreply.github.com>
2023-12-18 23:02:34 +0530

    Add files via upload

---
+++ PrimeNo.java
@@ -0,0 +1,16 @@
+
+public class PrimeNo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int i;
+      for(i=2;i<=20;i++)
+      {
+      if(i%2==0)
+      {
+    	  System.out.println(i+ " ");
+      }
+	}
+
+	}
+}
